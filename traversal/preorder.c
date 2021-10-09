1ab155595628e5906ecad5fe79a69a0f07919a49
Ramyashree shetty <ramyashreeshetty18042000@gmail.com>
2021-10-10 03:13:31 +0530

    commit

---
+++ traversal/preorder.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node * left;
+    struct node * right;
+}NODE;
+
+NODE * createnode(int val)
+{
+    NODE * newnode=(NODE *)malloc(sizeof(NODE));
+    newnode->data=val;
+    newnode->left=NULL;
+    newnode->right=NULL;
+    return newnode;
+}
+
+void preorder(NODE * root)
+{
+    if(root!=NULL)
+    {
+        printf("%d ",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main()
+{ 
+    NODE *p = createnode(4);
+    NODE *p1 = createnode(1);
+    NODE *p2 = createnode(6);
+    NODE *p3 = createnode(5);
+    NODE *p4 = createnode(2);
+
+
+    // Finally The tree looks like this:
+    //      4
+    //     / \
+    //    1   6
+    //   / \
+    //  5   2  
+
+
+    // Linking the root node with left and right children
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    preorder(p);
+    return 0;
+}
